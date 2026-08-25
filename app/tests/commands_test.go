d765e0dab465a388bec8b63ce7741e66cc0f70b6
waltereidi <walter-eidi@hotmail.com>
2026-08-24 21:41:41 -0300

    continuado teste de funcionamento

--- app/tests/commands_test.go
+++ app/tests/commands_test.go
@@ -4,12 +4,18 @@ import (
 	"testing"
 	"os"
 	"fmt"
+	"net/http"
+
 )
 
 
 func TestGetEnvironmentConfig(t *testing.T) {
 	println("============== iniciado test de comandos ")
 	
+	for i, arg := range os.Args {
+		fmt.Println(i, arg)
+	}
+
 	os.Args = []string{
     "caminho/do/programa",
     "Walter",
@@ -22,5 +28,24 @@ func TestGetEnvironmentConfig(t *testing.T) {
 	for i, arg := range os.Args {
 		fmt.Println(i, arg)
 	}
+
+}
+func TestGetEnvironmentConfig2(t *testing.T) {
+	println("============== iniciado test de comandos ")
+	PORT := ":8001"
+ 	arguments := os.Args
+    if len(arguments) != 1 {
+        PORT = ":" + arguments[1]
+    }
+    fmt.Println("Using port number: ", PORT)
+
+}
+func TestGetEnvironmentConfig3(t *testing.T) {
+	PORT := ":8001"
+	err := http.ListenAndServe(PORT, nil)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
 
 }
